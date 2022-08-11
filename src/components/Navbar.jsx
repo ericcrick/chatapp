bc38b6dfac038b87da0d441305c34113ee571869
ericnanaosei <eric.nana-osei@afrilogicsolutions.com>
2022-08-11 06:43:54 +0000

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export const Navbar = () => {
+  const styles = {
+    nav: `bg-gray-800 flex items-center justify-between h-4 p-4`,
+    heading: `text-2xl text-blue-400`
+  }
   return (
-    <div>Navbar</div>
+    <div className= {styles.nav}>
+      <h1 className= {styles.heading}>Chat App</h1>
+    </div>
   )
 }
